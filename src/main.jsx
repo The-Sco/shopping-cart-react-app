5404c7008f38455de0897bcd0166ef260cc0d1ce
hr6h6m <hr6h6m@192.168.1.60>
2026-05-08 14:40:26 +0300

    Fix base css - change path in the main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router";
 import routes from "./routes";
-import "./css/base.module.css";
+import "./css/base.css";
 
 const router = createBrowserRouter(routes);
 
